b0b5d4340efc336b9261e8e94515f5b7e87e8276
devsaifiix <devsaifiix@gmailcom>
2026-02-28 18:16:04 +0500

    Final commit

---
+++ 3.Veriables.py
@@ -0,0 +1,60 @@
+# # Python Variables
+# x = 5
+# name = "Saif"
+
+# print("x")
+# print("name")
+
+# # Below listed variable names are valid
+# age = 21
+# _colour = "lilac"
+# total_score = 90
+
+# # Assigning Values to Variables
+# x = 5
+# y = 3.14
+# z = "Hi"
+
+# # Dynamic Typing
+# x = 10
+# x = "Now a string"
+
+# # Multiple Assignments
+# a = b = c = 100
+# print(a, b, c)
+
+# # Assigning Different Values
+# x, y, z = 1, 2.5, "Python"
+# print(x, y, z)
+
+# # Type Casting a Variable
+# s = "10"
+# n = int(s)
+
+# count = 5
+# f = float(count)
+
+# age = 25
+# s2 = str(age)
+
+# print(n)
+# print(f)  
+# print(s2)
+# print(type(n))
+# print(type(f))
+# print(type(s2))
+
+# # Deleting a Variable
+# x = 10
+# del x
+# print(x)
+
+# # Swapping Two Variables
+# a, b = 5, 10
+# a, b = b, a
+# print(a, b)
+
+# Counting Characters in a String
+word = "Saif Tahir"
+length = len(word)
+print("Length of the word:", length)
