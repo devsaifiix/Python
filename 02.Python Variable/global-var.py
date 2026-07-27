84000c57d909acb67345db734645fc7e86aed1a9
devsaifiix <devsaifiix@gmail.com>
2026-07-27 11:46:51 +0500

    First Commit

--- 02.Python Variable/global-var.py
+++ 02.Python Variable/global-var.py
@@ -1,21 +1,23 @@
-# global variable
-x = "awesome"
+# # global variable
+# x = "awesome"
 
-def myfunc():
-    print("Python is " + x)
-myfunc()
+# def myfunc():
+#     print("Python is " + x)
+# myfunc()
 
-# Create a variable inside a function, with the same name as the global variable
-x = "awesome" # gobal variable
+# # Create a variable inside a function, with the same name as the global variable
+# x = "awesome" # gobal variable
 
-def myfunc():
-    x = "fantastic" # local variable
-    print("Python is " + x)
-myfunc()
+# def myfunc():
+#     x = "fantastic" # local variable
+#     print("Python is " + x)
+# myfunc()
 
-print("Python is " + x)
+# print("Python is " + x)
 
 # The global Keyword
+x = "awesome"
+
 def myfunc():
     global x
     x = "fantastic"
