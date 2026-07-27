4018d139b2e1fb66ef5a9acff2084578ece96073
devsaifiix <devsaifiix@gmail.com>
2026-07-27 10:00:54 +0500

    First Commit

---
+++ 02.Python Variable/creating-variables.py
@@ -0,0 +1,17 @@
+# simple creation
+x = 5
+y = "Saif"
+print(x)
+print(y)
+
+# Get the Type
+print(type(x))
+print(type(y))
+
+# Single or Double Quotes?
+x = "Saif"
+x = 'Saif' # both are the same
+
+# Case-Sensitive
+a = 4
+A = "Tahir" # A will not overwrite a 
