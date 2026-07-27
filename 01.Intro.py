845c29430dda20a34e79740cb54e45e4642edc28
devsaifiix <devsaifiix@gmail.com>
2026-07-27 08:12:32 +0500

    First Commit

---
+++ 01.Intro.py
@@ -0,0 +1,41 @@
+# Simple Syntax
+print("Hello, World!")
+
+# to check Python Version
+import sys
+print(sys.version)
+
+# Python Indentation
+if 5 > 2:
+  print("Five is greater than two!")
+
+# Python Variables
+x = 5
+y = "Hello, Saif!"
+print(x)
+print(y)
+
+# Comments
+#This is a single-line comment.
+print("Hello, World!")
+#This is a comment
+#written in
+#more than just one line
+"""
+This is a comment
+written in
+more than just one line
+"""
+print("Hello, World!")
+
+# Print Without a New Line
+print("Hello, World!", end=" ")
+print("I will print on the same line.")
+
+# Print Numbers
+print(3)
+print(358)
+print(50000)
+print(3 + 3)
+print(2 * 5)
+print("I am", 35, "years old.")
